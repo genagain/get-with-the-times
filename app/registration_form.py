2d8033856fd0bcf50aa0f15222ba966c07e15648
genagain <ohta.g@husky.neu.edu>
2016-03-05 00:49:44 -0500

    recreated form with the actual petition fields

--- app/registration_form.py
+++ app/registration_form.py
@@ -1,12 +1,9 @@
 from wtforms import Form, BooleanField, TextField, PasswordField, validators
 
 class RegistrationForm(Form):
-    username = TextField('Username', [validators.Length(min=4, max=25)])
-    email = TextField('Email Address', [validators.Length(min=4, max=35)])
-    password = PasswordField('New Password', [
-    validators.Required(),
-    validators.EqualTo('confirm', message='Passwords must match')
-    ])
-    confirm = PasswordField('Repeat Password')
+    first_name = TextField('First Name', [validators.Length(min=2, max=25)])
+    last_name = TextField('Last Name', [validators.Length(min=4, max=35)])
+    email = TextField('Email', [validators.Email()])
+    zip_code = TextField('Zip Code', [validators.Length(min=4, max=35)])
     accept_tos = BooleanField('I accept the TOS', [validators.Required()])
 
