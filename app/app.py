ba449ba43fbb87371d1e7229409cdb8d7ed3aedf
genagain <ohta.g@husky.neu.edu>
2016-03-16 13:34:48 -0400

    preparing to deploy to heroku

--- app/app.py
+++ app/app.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template, request
 from registration_form import RegistrationForm
-import ipdb
 
 app = Flask(__name__)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
-        ipdb.set_trace()
+    #if request.method == 'POST' and form.validate():
     return render_template('register.html', form=form)
 
 if __name__ == '__main__':
