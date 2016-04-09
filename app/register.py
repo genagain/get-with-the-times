1a32741c4393a5490b3fadee726e4cf03bccf418
genagain <ohta.g@husky.neu.edu>
2016-04-09 16:24:17 -0400

    deploying to heroku

--- app/register.py
+++ app/register.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/petition'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/petition'
+heroku = Heroku(app)
 db = SQLAlchemy(app)
 
 class Supporter(db.Model):
