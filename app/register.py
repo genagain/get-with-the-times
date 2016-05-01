6af996950c4e192d67d257aa52484ddeba934177
genagain <ohta.g@husky.neu.edu>
2016-05-01 19:09:38 -0400

    preparing to deploy to heroku

--- app/register.py
+++ app/register.py
@@ -2,13 +2,11 @@ from flask import Flask, render_template, request, redirect, flash
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import psycopg2
-import ipdb
-#from flask.ext.heroku import Heroku
+from flask.ext.heroku import Heroku
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/petition'
-#heroku = Heroku(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/petition'
+heroku = Heroku(app)
 db = SQLAlchemy(app)
 
 class Supporter(db.Model):
