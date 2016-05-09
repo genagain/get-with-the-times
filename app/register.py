0ab43554064771be6e664f46ecd7e584c57a603e
genagain <ohta.g@husky.neu.edu>
2016-05-08 20:09:31 -0400

    reverted using imports

--- app/register.py
+++ app/register.py
@@ -1,4 +1,4 @@
-from flask import flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
